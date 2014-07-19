48ef654c8417c7ac09e771a0d4df75416f4522cf
Neil Davis <neil.a.davis@gmail.com>
2014-07-19 15:10:31 +0100

    Initial Commit

---
+++ NDNavigatorExample/NDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NDAppDelegate.h
+//  NDNavigatorExample
+//
+//  Created by Neil Davis on 19/07/2014.
+//  Copyright (c) 2014 Neil Davis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NDNavigatorExampleTests/NDNavigatorExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  NDNavigatorExampleTests.m
+//  NDNavigatorExampleTests
+//
+//  Created by Neil Davis on 19/07/2014.
+//  Copyright (c) 2014 Neil Davis. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NDNavigatorExampleTests : XCTestCase
+
+@end
+
+@implementation NDNavigatorExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
